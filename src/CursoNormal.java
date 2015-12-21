1092d77e73b7cb9b6e17e4c58a7a84d7c90ae210
Bayons <mbayons@gmail.com>
2015-12-21 20:58:44 +0100

    Setters y Academia
    Falta rehacer la academia, esta simplemente empezada

--- src/CursoNormal.java
+++ src/CursoNormal.java
@@ -4,15 +4,15 @@ public class CursoNormal extends Curso {
 	private int nivel;
 	private CursoNormal cursoNivelSuperior;
 	private CursoNormal cursoNivelInferior;
-	
-	public CursoNormal(String idioma, int nivel, Calendar fInicio, Calendar fFinal, int horario, int maxAlumnos
-			,float precio, CursoNormal cursoNivelSuperior, CursoNormal cursoNivelInferior) {
+
+	public CursoNormal(String idioma, int nivel, Calendar fInicio, Calendar fFinal, int horario, int maxAlumnos,
+			float precio, CursoNormal cursoNivelSuperior, CursoNormal cursoNivelInferior) {
 		super(idioma, fInicio, fFinal, horario, maxAlumnos, precio);
 		this.nivel = nivel;
 		this.cursoNivelInferior = cursoNivelInferior;
 		this.cursoNivelSuperior = cursoNivelSuperior;
 	}
-	
+
 	/**
 	 * Getter del atributo <code>nivel</code>
 	 * 
@@ -21,16 +21,25 @@ public class CursoNormal extends Curso {
 	public int getNivel() {
 		return nivel;
 	}
-	
+
 	public CursoNormal getCursoSuperior() {
 		return this.cursoNivelSuperior;
 	}
-	
+
 	public CursoNormal getCursoInferior() {
 		return this.cursoNivelInferior;
 	}
-	
-	
-	
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+
+	public void setCursoSuperior(CursoNormal cursoSuperior) {
+		cursoNivelSuperior = cursoSuperior;
+	}
+
+	public void setCursoInferior(CursoNormal cursoInferior) {
+		cursoNivelInferior = cursoInferior;
+	}
 
 }
